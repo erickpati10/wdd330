74efcb7f863ddc112927ffe472a63092ee9c3a24
erickpati10 <pat21018@byui.edu>
2023-10-21 23:04:17 -0600

    js added

--- script/project.js
+++ script/project.js
@@ -3,61 +3,62 @@ const mealList = document.getElementById("meal");
 const mealDetailsContent = document.querySelector(".meal-details-content");
 const recipeCloseBtn = document.getElementById("recipe-close-btn");
 
-// event listeners
 searchBtn.addEventListener("click", getMealList);
 mealList.addEventListener("click", getMealRecipe);
 recipeCloseBtn.addEventListener("click", () => {
   mealDetailsContent.parentElement.classList.remove("showRecipe");
 });
 
-// get meal list that matches with the ingredients
-function getMealList() {
-  let searchInputTxt = document.getElementById("search-input").value.trim();
-  fetch(
-    `https://api.spoonacular.com/recipes/complexSearch?${searchInputTxt}apiKey=d1bfc342de7d4dbbb48c8660a2646f9e`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let html = "";
-      if (data.meals) {
-        data.meals.forEach((meal) => {
-          html += `
-                    <div class = "meal-item" data-id = "${meal.id}">
-                        <div class = "meal-img">
-                            <img src = "${meal.image}" alt = "food">
-                        </div>
-                        <div class = "meal-name">
-                            <h3>${meal.title}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
-                    </div>
-                `;
-        });
-        mealList.classList.remove("notFound");
-      } else {
-        html = "Sorry, we didn't find any meal!";
-        mealList.classList.add("notFound");
-      }
+async function getMealList() {
+  try {
+    let searchInputTxt = document.getElementById("search-input").value.trim();
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
+    );
+    const data = await response.json();
 
-      mealList.innerHTML = html;
-    });
-}
+    let html = "";
+    if (data.meals) {
+      data.meals.forEach((meal) => {
+        html += `
+          <div class="meal-item" data-id="${meal.idMeal}">
+            <div class="meal-img">
+              <img src="${meal.strMealThumb}" alt="food">
+            </div>
+            <div class="meal-name">
+              <h3>${meal.strMeal}</h3>
+              <a href="#" class="recipe-btn">Get Recipe</a>
+            </div>
+          </div>
+        `;
+      });
+      mealList.classList.remove("notFound");
+    } else {
+      html = "Sorry, we didn't find any meal!";
+      mealList.classList.add("notFound");
+    }
 
-// get recipe of the meal
-
-function getMealRecipe(e) {
-  e.preventDefault();
-  if (e.target.classList.contains("recipe-btn")) {
-    let mealItem = e.target.parentElement.parentElement;
-    fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=d1bfc342de7d4dbbb48c8660a2646f9e&recipes=${mealItem.dataset.id}`
-    )
-      .then((response) => response.json())
-      .then((data) => mealRecipeModal(data.meals));
+    mealList.innerHTML = html;
+  } catch (error) {
+    console.error("Error fetching data: ", error);
   }
 }
 
-// create a modal
+async function getMealRecipe(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("recipe-btn")) {
+    let mealItem = e.target.parentElement.parentElement;
+    try {
+      const response = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+      );
+      const data = await response.json();
+      mealRecipeModal(data.meals);
+    } catch (error) {
+      console.error("Error fetching recipe data: ", error);
+    }
+  }
+}
 
 function mealRecipeModal(meal) {
   console.log(meal);
@@ -72,7 +73,10 @@ function mealRecipeModal(meal) {
         <div class = "recipe-meal-img">
             <img src = "${meal.strMealThumb}" alt = "">
         </div>
-          `;
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
   mealDetailsContent.innerHTML = html;
   mealDetailsContent.parentElement.classList.add("showRecipe");
 }
